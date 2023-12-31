95179cdd0b2c1c98fc95c23b85a28091fbea58b7
junseo23 <hunseol03@gmail.com>
2023-12-31 13:45:26 +0900

    nav bar design

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Test from "@/components/Test"
+import Nav from "@/components/Nav"
 
 export default function Home() {
   return (
       <>
-        <h1>hello world</h1>
-        <Test></Test>
+        <h1>Hello world! this is home</h1>
       </>
     )
 }
