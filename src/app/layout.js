307c7311447d753107ae6083fd3e81af76da2461
junseo23 <hunseol03@gmail.com>
2024-01-01 14:29:04 +0900

    nav finish

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import Nav from '@/components/Nav'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'LOTTING',
@@ -11,11 +8,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-       <Nav />
-        {children}
-      </body>
-    </html>
+    <>
+      <Nav />
+      <main>{children}</main>
+    </>
   )
 }
