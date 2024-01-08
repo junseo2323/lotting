11525e39bca8d5874bda95918ad925639f720747
junseo23 <hunseol03@gmail.com>
2024-01-08 23:29:20 +0900

    input manage

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,28 @@
+"use client"
+
+import { Inputbox,PostInputbox,LongInputbox,DropInputbox,FileInputbox } from "@/components/Inputbox"
+import styles from "@/app/Home.module.scss"
 
 export default function Home() {
+  const handleChange = e => {
+    console.log(e.target.value)
+  }
+
   return (
-      <>
-      <h1>test</h1>
-      </>
+      <div >
+        <h1>123</h1>
+        <div className={styles.container}>
+          <PostInputbox />
+          <FileInputbox />
+          <Inputbox type="text" placeholder="이름" onChange={handleChange} />
+          <Inputbox type="date" placeholder="날짜" />
+          <Inputbox />
+          <Inputbox />
+          <LongInputbox />
+          <Inputbox />
+          <DropInputbox />
+          <Inputbox />
+        </div>    
+      </div>
     )
 }
