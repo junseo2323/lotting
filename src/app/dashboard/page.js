b04e6ac0fa96ab4a73b260e14b1974d08dfcd9d1
지나이 지나이 <lsj77205619@gmail.com>
2024-02-14 10:31:38 +0900

    dashboard 개발중

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,8 +1,317 @@
-export default function Dashboard() {
+import styles from "../../styles/Dashboard.module.scss";
+
+
+const Dashboard = () => {
     return (
-        <>
-          <h1>Dashboard</h1>
-        </>
-      )
-  }
-  
+      <div className={styles.container}>
+        <div className={styles.MainTitle}>
+          <div className={styles.ContractBody}>
+            <div className={styles.Contract}>
+
+              <div className={styles.ContractSum}>
+                <div className={styles.ContractValue}>
+                  <div className={styles.ContractIcons}>ds</div>
+                  <div className={styles.ContractName}>총 계약건수</div>
+                </div>
+                <div className={styles.ContractValue2}>
+                  <div className={styles.ContractVal1}>
+                    <div className={styles.Value}>1028</div>
+                  </div>
+                  <div className={styles.ContractVal2}>
+                    <div className={styles.Percentage}></div>
+                  </div>
+                </div>
+                <div className={styles.Bottominfo}>
+                  <div className={styles.BottomDate}>
+                    업데이트 : 2023년 12월 28일
+                  </div>
+                </div>
+              </div> 
+              {/* 덩어리 하나 */}
+            
+              <div className={styles.ContractSum}>
+                <div className={styles.ContractValue}>
+                  <div className={styles.ContractIcons}>ds</div>
+                  <div className={styles.ContractName}>완료인원</div>
+                </div>
+                <div className={styles.ContractValue2}>
+                  <div className={styles.ContractVal1}>
+                    <div className={styles.Value}>390</div>
+                  </div>
+                  <div className={styles.ContractVal2}>
+                    <div className={styles.Percentage}></div>
+                  </div>
+                </div>
+                <div className={styles.Bottominfo}>
+                  <div className={styles.BottomDate}>
+                    업데이트 : 2023년 12월 28일
+                  </div>
+                </div>
+              </div> 
+              {/* 덩어리 하나 */}
+
+            </div>
+            <div className={styles.Contract}>
+              <div className={styles.ContractSum}>
+                <div className={styles.ContractValue}>
+                  <div className={styles.ContractIcons}>ds</div>
+                  <div className={styles.ContractName}>계약금액</div>
+                </div>
+                <div className={styles.ContractValue2}>
+                  <div className={styles.ContractVal}>
+                    <div className={styles.Value}>12,390,000,000 ₩</div>
+                  </div>
+                </div>
+                <div className={styles.Bottominfo}>
+                  <div className={styles.BottomDate}>
+                    업데이트 : 2023년 12월 28일
+                  </div>
+                </div>
+              </div> 
+              {/* 덩어리 하나 */}
+
+              <div className={styles.ContractSum}>
+                <div className={styles.ContractValue}>
+                  <div className={styles.ContractIcons}>ds</div>
+                  <div className={styles.ContractName}>확입금액</div>
+                </div>
+                <div className={styles.ContractValue2}>
+                  <div className={styles.ContractVal}>
+                    <div className={styles.Value}>8,990,000,000 ₩</div>
+                  </div>
+                </div>
+                <div className={styles.Bottominfo}>
+                  <div className={styles.BottomDate}>
+                    업데이트 : 2023년 12월 28일
+                  </div>
+                </div>
+              </div> 
+              {/* 덩어리 하나 */}
+
+            </div>
+
+
+            
+            <div className={styles.Client}>
+              <div className={styles.ClientBody}>
+                <div className={styles.ClientContents}>
+                  <div className={styles.ClientTitle}>
+                    <div className={styles.ClientTitle1}>
+                      <div className={styles.Titlefont}>
+                        미납자 명단
+                      </div>
+                    </div>
+                    <div className={styles.ClientTitle2}>
+                      <div className={styles.InfoBotton}>
+                        <div className={styles.BottonInfo}>정보입력</div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className={styles.ClientCategory}>
+                    <div className={styles.Number}>
+                      <div className={styles.Categoryfont}>
+                        관리번호
+                      </div>
+                    </div>
+                    <div className={styles.Name}>
+                      <div className={styles.Categoryfont}>
+                        성명
+                      </div>
+                    </div>
+                    <div className={styles.Cha}>
+                      <div className={styles.Categoryfont}>
+                        최종납부
+                      </div>
+                    </div>
+                    <div className={styles.Dong}>
+                      <div className={styles.Categoryfont}>
+                        임시동호
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <div className={styles.ClientValue}>
+                    <div className={styles.Number}>
+                      <div className={styles.Valuefont}>
+                        123455
+                      </div>
+                    </div>
+                    <div className={styles.Name}>
+                      <div className={styles.Valuefont}>
+                        이승준
+                      </div>
+                    </div>
+                    <div className={styles.Cha}>
+                      <div className={styles.Valuefont}>
+                        4차
+                      </div>
+                    </div>
+                    <div className={styles.Dong}>
+                      <div className={styles.Valuefont}>
+                        84A-사-1
+                      </div>
+                    </div>
+                  </div>
+                  {/* 덩어리 하나 */}
+
+                  <div className={styles.ClientValue}>
+                    <div className={styles.Number}>
+                      <div className={styles.Valuefont}>
+                        123455
+                      </div>
+                    </div>
+                    <div className={styles.Name}>
+                      <div className={styles.Valuefont}>
+                        이승준
+                      </div>
+                    </div>
+                    <div className={styles.Cha}>
+                      <div className={styles.Valuefont}>
+                        4차
+                      </div>
+                    </div>
+                    <div className={styles.Dong}>
+                      <div className={styles.Valuefont}>
+                        84A-사-1
+                      </div>
+                    </div>
+                  </div>
+                  {/* 덩어리 하나 */}
+
+                  <div className={styles.ClientValue}>
+                    <div className={styles.Number}>
+                      <div className={styles.Valuefont}>
+                        123455
+                      </div>
+                    </div>
+                    <div className={styles.Name}>
+                      <div className={styles.Valuefont}>
+                        이승준
+                      </div>
+                    </div>
+                    <div className={styles.Cha}>
+                      <div className={styles.Valuefont}>
+                        4차
+                      </div>
+                    </div>
+                    <div className={styles.Dong}>
+                      <div className={styles.Valuefont}>
+                        84A-사-1
+                      </div>
+                    </div>
+                  </div>
+                  {/* 덩어리 하나 */}
+
+                  <div className={styles.ClientValue}>
+                    <div className={styles.Number}>
+                      <div className={styles.Valuefont}>
+                        123455
+                      </div>
+                    </div>
+                    <div className={styles.Name}>
+                      <div className={styles.Valuefont}>
+                        이승준
+                      </div>
+                    </div>
+                    <div className={styles.Cha}>
+                      <div className={styles.Valuefont}>
+                        4차
+                      </div>
+                    </div>
+                    <div className={styles.Dong}>
+                      <div className={styles.Valuefont}>
+                        84A-사-1
+                      </div>
+                    </div>
+                  </div>
+                  {/* 덩어리 하나 */}
+
+                  <div className={styles.ClientValue}>
+                    <div className={styles.Number}>
+                      <div className={styles.Valuefont}>
+                        123455
+                      </div>
+                    </div>
+                    <div className={styles.Name}>
+                      <div className={styles.Valuefont}>
+                        이승준
+                      </div>
+                    </div>
+                    <div className={styles.Cha}>
+                      <div className={styles.Valuefont}>
+                        4차
+                      </div>
+                    </div>
+                    <div className={styles.Dong}>
+                      <div className={styles.Valuefont}>
+                        84A-사-1
+                      </div>
+                    </div>
+                  </div>
+                  {/* 덩어리 하나 */}
+
+                  <div className={styles.ClientValue}>
+                    <div className={styles.Number}>
+                      <div className={styles.Valuefont}>
+                        123455
+                      </div>
+                    </div>
+                    <div className={styles.Name}>
+                      <div className={styles.Valuefont}>
+                        이승준
+                      </div>
+                    </div>
+                    <div className={styles.Cha}>
+                      <div className={styles.Valuefont}>
+                        4차
+                      </div>
+                    </div>
+                    <div className={styles.Dong}>
+                      <div className={styles.Valuefont}>
+                        84A-사-1
+                      </div>
+                    </div>
+                  </div>
+                  {/* 덩어리 하나 */}
+
+                  <div className={styles.ClientValue}>
+                    <div className={styles.Number}>
+                      <div className={styles.Valuefont}>
+                        123455
+                      </div>
+                    </div>
+                    <div className={styles.Name}>
+                      <div className={styles.Valuefont}>
+                        이승준
+                      </div>
+                    </div>
+                    <div className={styles.Cha}>
+                      <div className={styles.Valuefont}>
+                        4차
+                      </div>
+                    </div>
+                    <div className={styles.Dong}>
+                      <div className={styles.Valuefont}>
+                        84A-사-1
+                      </div>
+                    </div>
+                  </div>
+                  {/* 덩어리 하나 */}
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className={styles.Date}></div>
+        </div>
+        <div className={styles.Notice}></div>
+      </div>
+      );
+  };
+
+export default Dashboard;
