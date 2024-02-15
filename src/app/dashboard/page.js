8d047974c2c8e97df9b7e235d65b0ac725b7cabb
지나이 지나이 <lsj77205619@gmail.com>
2024-02-15 13:58:02 +0900

    dashboard 완성?

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,4 +1,10 @@
 import styles from "../../styles/Dashboard.module.scss";
+import dash1 from "../../img/dash1.png";
+import dash2 from "../../img/dash2.png";
+import dash3 from "../../img/dash3.png";
+import dash4 from "../../img/dash4.png";
+import up from "../../img/up.png";
+import calendar from "../../img/calendar.png";
 
 
 const Dashboard = () => {
@@ -10,7 +16,9 @@ const Dashboard = () => {
 
               <div className={styles.ContractSum}>
                 <div className={styles.ContractValue}>
-                  <div className={styles.ContractIcons}>ds</div>
+                  <div className={styles.ContractIcons}>
+                    <img src={dash1} className={styles.ContractImg}></img>
+                  </div>
                   <div className={styles.ContractName}>총 계약건수</div>
                 </div>
                 <div className={styles.ContractValue2}>
@@ -18,7 +26,9 @@ const Dashboard = () => {
                     <div className={styles.Value}>1028</div>
                   </div>
                   <div className={styles.ContractVal2}>
-                    <div className={styles.Percentage}></div>
+                    <div className={styles.Percentage_G}>
+                      <img src={up} className={styles.UpImg}></img>
+                    </div>
                   </div>
                 </div>
                 <div className={styles.Bottominfo}>
@@ -31,7 +41,9 @@ const Dashboard = () => {
             
               <div className={styles.ContractSum}>
                 <div className={styles.ContractValue}>
-                  <div className={styles.ContractIcons}>ds</div>
+                  <div className={styles.ContractIcons}>
+                    <img src={dash2} className={styles.ContractImg}></img>
+                  </div>
                   <div className={styles.ContractName}>완료인원</div>
                 </div>
                 <div className={styles.ContractValue2}>
@@ -54,7 +66,9 @@ const Dashboard = () => {
             <div className={styles.Contract}>
               <div className={styles.ContractSum}>
                 <div className={styles.ContractValue}>
-                  <div className={styles.ContractIcons}>ds</div>
+                  <div className={styles.ContractIcons}>
+                    <img src={dash3} className={styles.ContractImg}></img>
+                  </div>
                   <div className={styles.ContractName}>계약금액</div>
                 </div>
                 <div className={styles.ContractValue2}>
@@ -72,7 +86,9 @@ const Dashboard = () => {
 
               <div className={styles.ContractSum}>
                 <div className={styles.ContractValue}>
-                  <div className={styles.ContractIcons}>ds</div>
+                  <div className={styles.ContractIcons}>
+                    <img src={dash4} className={styles.ContractImg}></img>
+                  </div>
                   <div className={styles.ContractName}>확입금액</div>
                 </div>
                 <div className={styles.ContractValue2}>
@@ -103,7 +119,7 @@ const Dashboard = () => {
                     </div>
                     <div className={styles.ClientTitle2}>
                       <div className={styles.InfoBotton}>
-                        <div className={styles.BottonInfo}>정보입력</div>
+                        <div className={styles.Bottonfont}>정보입력</div>
                       </div>
                     </div>
                   </div>
@@ -307,9 +323,103 @@ const Dashboard = () => {
 
 
           </div>
-          <div className={styles.Date}></div>
+          <div className={styles.Date}>
+            <div className={styles.DateTitle}>
+              <div className={styles.DateTitle1}>
+                <div className={styles.DateTitleName}>일정관리</div>
+              </div>
+              <div className={styles.DateTitle2}>
+                <div className={styles.DateIcon}>
+                  <img src={calendar} className={styles.ContractImg}></img>
+                </div>
+              </div>
+            </div>
+            <div className={styles.Calendar}></div>
+            <div className={styles.Line}></div>
+            <div className={styles.Today}>
+              <div className={styles.TodayTitle}>
+                <div className={styles.TodayTitle1}>
+                  <div className={styles.TodayTitleName}>오늘의 일정</div>
+                </div>
+                <div className={styles.TodayTitle2}></div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.Notice}></div>
+
+        <div className={styles.Notice}>
+          <div className={styles.NoticeTitle}>
+            <div className={styles.NoticeTitle1}>
+              <div className={styles.NoticeName}>공지사항</div>
+            </div>
+            <div className={styles.NoticeTitle2}>
+              <div className={styles.NoticeBotton}>
+                <div className={styles.NoticeBottonFont}>View All</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.NoticeCategory}>
+            <div className={styles.NoticeCategory1}>
+              <div className={styles.NoticeCategoryFont}>제목</div>
+            </div>
+            <div className={styles.NoticeCategory2}>
+              <div className={styles.NoticeCategoryFont}>작성자</div>
+            </div>
+            <div className={styles.NoticeCategory3}>
+              <div className={styles.NoticeCategoryFont}>사항</div>
+            </div>
+            <div className={styles.NoticeCategory4}>
+              <div className={styles.NoticeCategoryFont}>작성일자</div>
+            </div>
+            <div className={styles.NoticeCategory5}>
+              <div className={styles.NoticeCategoryFont}>상태</div>
+            </div>
+          </div>
+
+          <div className={styles.NoticeContent}>
+            <div className={styles.NoticeCategory1}>
+              <div className={styles.NoticeContentFont}>사이트 개설 안내</div>
+            </div>
+            <div className={styles.NoticeCategory2}>
+              <div className={styles.NoticeContentFont}>오준서</div>
+            </div>
+            <div className={styles.NoticeCategory3}>
+              <div className={styles.NoticeContentFont}>일반</div>
+            </div>
+            <div className={styles.NoticeCategory4}>
+              <div className={styles.NoticeContentFont}>23-12-28 09:27 AM</div>
+            </div>
+            <div className={styles.NoticeCategory5}>
+              <div className={styles.ContentState_G}>
+                <div className={styles.G_Font}>해결됨</div>
+              </div>
+            </div>
+          </div>
+          {/* 내용 한 덩어리 */}
+
+          <div className={styles.NoticeContent}>
+            <div className={styles.NoticeCategory1}>
+              <div className={styles.NoticeContentFont}>[긴급] 시스템 점검 안내 (12/29 8Am 부터)</div>
+            </div>
+            <div className={styles.NoticeCategory2}>
+              <div className={styles.NoticeContentFont}>오준서</div>
+            </div>
+            <div className={styles.NoticeCategory3}>
+              <div className={styles.NoticeContentFont}>긴급</div>
+            </div>
+            <div className={styles.NoticeCategory4}>
+              <div className={styles.NoticeContentFont}>23-12-28 09:27 AM</div>
+            </div>
+            <div className={styles.NoticeCategory5}>
+              <div className={styles.ContentState_B}>
+                <div className={styles.B_Font}>진행중</div>
+              </div>
+            </div>
+          </div>
+          {/* 내용 한 덩어리 */}
+          
+
+        </div>
       </div>
       );
   };
---
+++ src/app/inputclient/page.js
@@ -0,0 +1,11 @@
+import styles from "../../styles/Inputclient.module.scss";
+
+const Inputclient = () =>{
+    return(
+        <div className="Container">
+            <div className="MainBody"></div>
+        </div>
+    );
+};
+
+export default Inputclient;
