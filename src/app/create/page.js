82f32a08248db8e3141dbec1a5003907248faa1d
junseo23 <hunseol03@gmail.com>
2024-02-26 22:07:46 +0900

    create modify

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -36,11 +36,11 @@ export default function Create() {
             <h3>분양인 정보</h3>
             <div className={styles.content_container}>
               <span>관리번호 : 123456</span> <h1></h1>
-              <Inputbox type="text" placeholder="이름" register={register('')} />
-              <Inputbox type="password" placeholder="주민번호" register={register('')} />
-              <Inputbox type="phone" placeholder="휴대폰 번호" register={register('')} />
-              <Inputbox type="email" placeholder="이메일" register={register('')}/>
-              <Inputbox type="text" placeholder="가입경로" register={register('')} />
+              <Inputbox type="text" placeholder="이름" register={register('name')} />
+              <Inputbox type="password" placeholder="주민번호" register={register('id')} />
+              <Inputbox type="phone" placeholder="휴대폰 번호" register={register('phone')} />
+              <Inputbox type="email" placeholder="이메일" register={register('email')}/>
+              <Inputbox type="text" placeholder="가입경로" register={register('path')} />
               <h1></h1>
               <DropInputbox list={banklist} register={register('')}/>
               <Inputbox type="text" placeholder="계좌번호" register={register('')} />
@@ -53,23 +53,23 @@ export default function Create() {
 
             <h3>관리 정보</h3>
             <div className={styles.content_container}>
-              <DropInputbox list={banklist}/> 
-              <DropInputbox list={banklist}/>
-              <DropInputbox list={banklist}/>
-              <DropInputbox list={banklist}/>
+              <DropInputbox list={banklist} register={register('')}/> 
+              <DropInputbox list={banklist} register={register('')}/>
+              <DropInputbox list={banklist} register={register('')}/>
+              <DropInputbox list={banklist} register={register('')}/>
               <Spanbox>임시동호 : </Spanbox>
-              <Inputbox type="text" placeholder="가입차순" onChange={handleChange} />
+              <Inputbox type="text" placeholder="가입차순" onChange={handleChange} register={register('')}/>
 
-              <Inputbox type="date" date_placeholder="가입일자" onChange={handleChange} />
-              <Inputbox type="date" date_placeholder="신탁사 제출일자" onChange={handleChange} />
-              <Inputbox type="email" placeholder="이메일" onChange={handleChange} />
-              <Inputbox type="text" placeholder="가입경로" onChange={handleChange} />
+              <Inputbox type="date" date_placeholder="가입일자" onChange={handleChange}register={register('')} />
+              <Inputbox type="date" date_placeholder="신탁사 제출일자" onChange={handleChange}register={register('')} />
+              <Inputbox type="email" placeholder="이메일" onChange={handleChange} register={register('')}/>
+              <Inputbox type="text" placeholder="가입경로" onChange={handleChange} register={register('')}/>
               <h1></h1>
-              <Inputbox type="text" placeholder="계좌번호" onChange={handleChange} />
-              <Inputbox type="text" placeholder="예금주" onChange={handleChange} />
-              <DropInputbox list={banklist}/>
-              <PostInputbox placeholder="우편물 수령주소"/>
-              <PostInputbox placeholder="주소지"/>
+              <Inputbox type="text" placeholder="계좌번호" onChange={handleChange} register={register('')}/>
+              <Inputbox type="text" placeholder="예금주" onChange={handleChange} register={register('')}/>
+              <DropInputbox list={banklist} register={register('')}/>
+              <PostInputbox placeholder="우편물 수령주소" register={register('')}/>
+              <PostInputbox placeholder="주소지" register={register('')}/>
             </div> 
           </form>
       </div>
