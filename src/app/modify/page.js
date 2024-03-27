75d77f452ff12d74a9e783fa7254ec3e12d5232c
지나이 지나이 <lsj77205619@gmail.com>
2024-03-27 17:21:18 +0900

    이것저것.. 은행 등등 대출페이지 완성중

--- src/app/modify/page.js
+++ src/app/modify/page.js
@@ -7,6 +7,7 @@ import { namesearchSelector } from "@/utils/selector";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRecoilValue, useRecoilValueLoadable, useSetRecoilState } from "recoil";
+import { banklist,sintacklist,typeidlist,typelist,grouplist,turnlist } from "@/components/droplistdata"
 
 
 const SearchList = (name) => {
@@ -39,138 +40,6 @@ const SearchList = (name) => {
 
 }
 
-const typelist = [
-  {
-    "value" : "44가",
-    "item" : "44가"
-  },
-  {
-    "value" : "44나",
-    "item" : "44나"
-  },
-  {
-    "value" : "44다",
-    "item" : "44다"
-  },
-  {
-    "value" : "49가",
-    "item" : "49가"
-  },
-  {
-    "value" : "49나",
-    "item" : "49나"
-  },
-  {
-    "value" : "49다",
-    "item" : "49다"
-  },
-  {
-    "value" : "70가",
-    "item" : "70가"
-  },
-  {
-    "value" : "70나",
-    "item" : "70나"
-  },
-  {
-    "value" : "70다",
-    "item" : "70다"
-  },
-  {
-    "value" : "84A가",
-    "item" : "84A가"
-  },
-  {
-    "value" : "84A나",
-    "item" : "84A나"
-  },
-  {
-    "value" : "84A다",
-    "item" : "84A다"
-  },
-  {
-    "value" : "84A라",
-    "item" : "84A라"
-  },
-  {
-    "value" : "84A마",
-    "item" : "84A마"
-  },
-  {
-    "value" : "84A바",
-    "item" : "84A바"
-  },
-  {
-    "value" : "84A사",
-    "item" : "84A사"
-  },
-  {
-    "value" : "84B가",
-    "item" : "84B가"
-  },
-  {
-    "value" : "84B나",
-    "item" : "84B나"
-  },
-  {
-    "value" : "84B다",
-    "item" : "84B다"
-  },
-  {
-    "value" : "84B라",
-    "item" : "84B라"
-  },
-  {
-    "value" : "84B마",
-    "item" : "84B마"
-  },
-  {
-    "value" : "84B바",
-    "item" : "84B바"
-  },
-  {
-    "value" : "84B사",
-    "item" : "84B사"
-  }
-]
-
-const grouplist = [
-  {
-    "value" : "가",
-    "item" : "가"
-  },
-  {
-    "value" : "나",
-    "item" : "나"
-  },
-  {
-    "value" : "다",
-    "item" : "다"
-  },
-  {
-    "value" : "라",
-    "item" : "라"
-  },
-  {
-    "value" : "마",
-    "item" : "마"
-  },
-  {
-    "value" : "사",
-    "item" : "사"
-  },
-]
-
-const turnlist = [
-  {
-    "value" : 1,
-    "item" : 1
-  },
-  {
-    "value" : 2,
-    "item" : 2
-  },
-]
 
 export default function Modify() {
   const [name,setName] = useState("");
