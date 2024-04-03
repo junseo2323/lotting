1ec3f08af3d0f4bfc16983d5f3156d2d0c57d12e
junseo23 <hunseol03@gmail.com>
2024-04-03 15:12:37 +0900

    240403

--- next.config.js
+++ next.config.js
@@ -45,10 +45,28 @@ const nextConfig = {
 	  return [
 		{
 		  source: "/api/:path*",
-		  destination: "http://3.39.167.172:8000/api/:path*",
+		  destination: "http://localhost:8000/api/:path*",
 		},
 	  ];
 	},  
 }
+
+module.exports = {
+	async headers() {
+	  return [
+		{
+		  // matching all API routes
+		  source: "/api/:path*",
+		  headers: [
+			{ key: "Access-Control-Allow-Credentials", value: "true" },
+			{ key: "Access-Control-Allow-Origin", value: "*" },
+			{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+			{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+		  ]
+		}
+	  ]
+	}
+  };
+  
 
 module.exports = nextConfig
