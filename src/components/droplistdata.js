00cedbcb18a6f13fa5f0b6c69a4c51b49f91f6e2
Audora <gy2553@naver.com>
2024-07-13 08:20:37 +0900

    유저생성오류방지 droplistdata 수정

--- src/components/droplistdata.js
+++ src/components/droplistdata.js
@@ -1,210 +1,201 @@
-export const banklist = [{
-"value" : "SHINHAN",
-"item" : "신한은행"
-},
-{
-"value" : "IBK",
-"item" : "IBK기업은행"
-},
-{
-"value" : "KOOKMIN",
-"item" : "KB국민은행"
-},
-{
-"value" : "SUHYUP",
-"item" : "수협은행"
-},
-{
-"value" : "NH",
-"item" : "농협은행"
-},
-{
-"value" : "HANA",
-"item" : "KEB하나은행"
-},
-{
-"value" : "SHIN",
-"item" : "신한은행"
-},
-{
-"value" : "IBK",
-"item" : "IBK기업은행"
-},
-{
-"value" : "SC",
-"item" : "SC제일은행"
-},
-{
-"value" : "HWAN",
-"item" : "외환은행"
-},
-{
-"value" : "CITY",
-"item" : "한국시티은행"
-},
-{
-"value" : "KN",
-"item" : "경남은행"
-},
-{
-"value" : "GJ",
-"item" : "광주은행"
-},
-{
-"value" : "DG",
-"item" : "대구은행"
-},
-{
-"value" : "BS",
-"item" : "부산은행"
-},
-{
-"value" : "JB",
-"item" : "전북은행"
-},
-{
-"value" : "JEJU",
-"item" : "제주은행"
-},
-{
-"value" : "INDUSTRY",
-"item" : "한국산업은행"
-},
-{
-"value" : "EXPORT",
-"item" : "한국수출은행"
-},
-]
-  
-export const sintacklist = [{
-"value" : "MOGUNGHWA",
-"item" : "무궁화신탁"
-},
-{
-"value" : "KYOBO",
-"item" : "교보자산신탁"
-},
-{
-"value" : "DASIN",
-"item" : "대신자산신탁"
-},
-{
-"value" : "SINYOUNG",
-"item" : "신영부동산신탁"
-}
-]
+export const banklist = [
+  {
+    value: "SHINHAN",
+    item: "신한은행",
+  },
+  {
+    value: "IBK",
+    item: "IBK기업은행",
+  },
+  {
+    value: "KOOKMIN",
+    item: "KB국민은행",
+  },
+  {
+    value: "SUHYUP",
+    item: "수협은행",
+  },
+  {
+    value: "NH",
+    item: "농협은행",
+  },
+  {
+    value: "HANA",
+    item: "KEB하나은행",
+  },
+  {
+    value: "SHIN",
+    item: "신한은행",
+  },
+  {
+    value: "IBK",
+    item: "IBK기업은행",
+  },
+  {
+    value: "SC",
+    item: "SC제일은행",
+  },
+  {
+    value: "HWAN",
+    item: "외환은행",
+  },
+  {
+    value: "CITY",
+    item: "한국시티은행",
+  },
+  {
+    value: "KN",
+    item: "경남은행",
+  },
+  {
+    value: "GJ",
+    item: "광주은행",
+  },
+  {
+    value: "DG",
+    item: "대구은행",
+  },
+  {
+    value: "BS",
+    item: "부산은행",
+  },
+  {
+    value: "JB",
+    item: "전북은행",
+  },
+  {
+    value: "JEJU",
+    item: "제주은행",
+  },
+  {
+    value: "INDUSTRY",
+    item: "한국산업은행",
+  },
+  {
+    value: "EXPORT",
+    item: "한국수출은행",
+  },
+];
 
-export const typeidlist = [{
-"value" : "1-1차",
-"item" : "1-1차"
-},
-{
-"value" : "1-2차",
-"item" : "1-2차"
-},
-{
-"value" : "1-3차",
-"item" : "1-3차"
-},
-{
-"value" : "1-4차",
-"item" : "1-4차"
-},
-{
-"value" : "1-5차",
-"item" : "1-5차"
-},
-{
-"value" : "2차",
-"item" : "2차"
-}
-]
+export const sintacklist = [
+  {
+    value: "MOGUNGHWA",
+    item: "무궁화신탁",
+  },
+  {
+    value: "KYOBO",
+    item: "교보자산신탁",
+  },
+  {
+    value: "DASIN",
+    item: "대신자산신탁",
+  },
+  {
+    value: "SINYOUNG",
+    item: "신영부동산신탁",
+  },
+];
 
-export  const typelist = [
-{
-    "value":"타입",
-    "item":"타입"
-},
-{
-"value" : "44",
-"item" : "44"
-},
-{
-"value" : "49",
-"item" : "49"
-},
-{
-"value" : "70",
-"item" : "70"
-},
-{
-"value" : "84A",
-"item" : "84A"
-},
-{
-"value" : "84B",
-"item" : "84B"
-}
-]
+export const typeidlist = [
+  {
+    value: "1-1차",
+    item: "1-1차",
+  },
+  {
+    value: "1-2차",
+    item: "1-2차",
+  },
+  {
+    value: "1-3차",
+    item: "1-3차",
+  },
+  {
+    value: "1-4차",
+    item: "1-4차",
+  },
+  {
+    value: "1-5차",
+    item: "1-5차",
+  },
+  {
+    value: "2차",
+    item: "2차",
+  },
+];
 
-export  const grouplist = [
-{
-    "value":"군",
-    "item":"군"
-},
-{
-"value" : "가",
-"item" : "가"
-},
-{
-"value" : "나",
-"item" : "나"
-},
-{
-"value" : "다",
-"item" : "다"
-},
-{
-"value" : "라",
-"item" : "라"
-},
-{
-"value" : "마",
-"item" : "마"
-},
-{
-"value" : "바",
-"item" : "바"
-},
-{
-"value" : "사",
-"item" : "사"
-},
-]
+export const typelist = [
+  {
+    value: "44",
+    item: "44",
+  },
+  {
+    value: "49",
+    item: "49",
+  },
+  {
+    value: "70",
+    item: "70",
+  },
+  {
+    value: "84A",
+    item: "84A",
+  },
+  {
+    value: "84B",
+    item: "84B",
+  },
+];
+
+export const grouplist = [
+  {
+    value: "가",
+    item: "가",
+  },
+  {
+    value: "나",
+    item: "나",
+  },
+  {
+    value: "다",
+    item: "다",
+  },
+  {
+    value: "라",
+    item: "라",
+  },
+  {
+    value: "마",
+    item: "마",
+  },
+  {
+    value: "바",
+    item: "바",
+  },
+  {
+    value: "사",
+    item: "사",
+  },
+];
 
 export const turnlist = [
-{
-    "value":"순번",
-    "item":"순번"
-},
-{
-"value" : "1",
-"item" : "1"
-},
-{
-"value" : "2",
-"item" : "2"
-},
-{
-"value" : "3",
-"item" : "3"
-},
-{
-"value" : "4",
-"item" : "4"
-},
-{
-"value" : "5",
-"item" : "5"
-}
-]
+  {
+    value: "1",
+    item: "1",
+  },
+  {
+    value: "2",
+    item: "2",
+  },
+  {
+    value: "3",
+    item: "3",
+  },
+  {
+    value: "4",
+    item: "4",
+  },
+  {
+    value: "5",
+    item: "5",
+  },
+];
