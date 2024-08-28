284329e7acab02569a163a33fac4e1baa1aa8ea5
Audora <gy2553@naver.com>
2024-08-28 16:24:56 +0900

    애니메이션 살짝 넣어봄 ㅎ

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,12 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { AiFillAppstore, AiOutlineAppstore, AiOutlineDollar, AiFillDollarCircle } from "react-icons/ai";
+import {
+  AiFillAppstore,
+  AiOutlineAppstore,
+  AiOutlineDollar,
+  AiFillDollarCircle,
+} from "react-icons/ai";
 import { HiOutlineUsers, HiUsers } from "react-icons/hi2";
 import LOGO from "@/img/logo.png";
 import styles from "../styles/Nav.module.scss";
@@ -13,66 +18,114 @@ const iconstyle = { fontSize: "1.5em", paddingRight: "10%", paddingLeft: "7%" };
 
 const Nav = () => {
   const pathname = usePathname();
-  const splitpath = pathname.split('/');
+  const splitpath = pathname.split("/");
 
   return (
     <div className={styles.container}>
       <Link href="/dashboard">
         <Image src={LOGO} className={styles.logostyle} alt={"logo"} />
       </Link>
-      <ul className={styles.listconatiner}>
+      <div className={styles.listContainer}>
         <Link href="/dashboard">
-          <li className={splitpath[1] === "dashboard" ? styles.select : styles.nonselect}>
-            <div className={styles.innerconaainer}>
-              {splitpath[1] === "dashboard" ? (<AiFillAppstore style={iconstyle} />) : (<AiOutlineAppstore style={iconstyle} />)}
+          <div
+            className={
+              splitpath[1] === "dashboard" ? styles.select : styles.nonselect
+            }
+          >
+            <div className={styles.innerContainer}>
+              {splitpath[1] === "dashboard" ? (
+                <AiFillAppstore style={iconstyle} />
+              ) : (
+                <AiOutlineAppstore style={iconstyle} />
+              )}
               <span className={styles.innertext}>대시보드</span>
             </div>
-          </li>
+          </div>
         </Link>
-        <li className={splitpath[1] === "search" ? styles.select : styles.nonselect}>
-          <div className={styles.innerconaainer}>
-            {splitpath[1] === "search" ? (<HiUsers style={iconstyle} />) : (<HiOutlineUsers style={iconstyle} />)}
+        <div
+          className={
+            splitpath[1] === "search" ? styles.select : styles.nonselect
+          }
+        >
+          <div className={styles.innerContainer}>
+            {splitpath[1] === "search" ? (
+              <HiUsers style={iconstyle} />
+            ) : (
+              <HiOutlineUsers style={iconstyle} />
+            )}
             <span className={styles.innertext}>회원 정보 검색</span>
           </div>
-          <ul className={styles.dropdownMenu}>
-            <li><Link href="/search">정계약</Link></li>
-            <li><Link href="/search/modify">수정</Link></li>
-            <li><Link href="/search/cancel">해지</Link></li>
-          </ul>
-        </li>
+          <div className={styles.dropdownMenu}>
+            <Link href="/search">정계약</Link>
+            <Link href="/search/modify">수정</Link>
+            <Link href="/search/cancel">해지</Link>
+          </div>
+        </div>
         <Link href="/create">
-          <li className={splitpath[1] === "create" ? styles.select : styles.nonselect}>
-            <div className={styles.innerconaainer}>
-              {splitpath[1] === "create" ? (<HiUsers style={iconstyle} />) : (<HiOutlineUsers style={iconstyle} />)}
+          <div
+            className={
+              splitpath[1] === "create" ? styles.select : styles.nonselect
+            }
+          >
+            <div className={styles.innerContainer}>
+              {splitpath[1] === "create" ? (
+                <HiUsers style={iconstyle} />
+              ) : (
+                <HiOutlineUsers style={iconstyle} />
+              )}
               <span className={styles.innertext}>회원 정보 입력</span>
             </div>
-          </li>
+          </div>
         </Link>
         <Link href="/modify">
-          <li className={splitpath[1] === "modify" ? styles.select : styles.nonselect}>
-            <div className={styles.innerconaainer}>
-              {splitpath[1] === "modify" ? (<HiUsers style={iconstyle} />) : (<HiOutlineUsers style={iconstyle} />)}
+          <div
+            className={
+              splitpath[1] === "modify" ? styles.select : styles.nonselect
+            }
+          >
+            <div className={styles.innerContainer}>
+              {splitpath[1] === "modify" ? (
+                <HiUsers style={iconstyle} />
+              ) : (
+                <HiOutlineUsers style={iconstyle} />
+              )}
               <span className={styles.innertext}>회원 정보 수정</span>
             </div>
-          </li>
+          </div>
         </Link>
         <Link href="/inputmoney">
-          <li className={splitpath[1] === "inputmoney" ? styles.select : styles.nonselect}>
-            <div className={styles.innerconaainer}>
-              {splitpath[1] === "inputmoney" ? (<AiFillDollarCircle style={iconstyle} />) : (<AiOutlineDollar style={iconstyle} />)}
+          <div
+            className={
+              splitpath[1] === "inputmoney" ? styles.select : styles.nonselect
+            }
+          >
+            <div className={styles.innerContainer}>
+              {splitpath[1] === "inputmoney" ? (
+                <AiFillDollarCircle style={iconstyle} />
+              ) : (
+                <AiOutlineDollar style={iconstyle} />
+              )}
               <span className={styles.innertext}>납입금 관리</span>
             </div>
-          </li>
+          </div>
         </Link>
         <Link href="/control">
-          <li className={splitpath[1] === "control" ? styles.select : styles.nonselect}>
-            <div className={styles.innerconaainer}>
-              {splitpath[1] === "control" ? (<AiFillDollarCircle style={iconstyle} />) : (<AiOutlineDollar style={iconstyle} />)}
+          <div
+            className={
+              splitpath[1] === "control" ? styles.select : styles.nonselect
+            }
+          >
+            <div className={styles.innerContainer}>
+              {splitpath[1] === "control" ? (
+                <AiFillDollarCircle style={iconstyle} />
+              ) : (
+                <AiOutlineDollar style={iconstyle} />
+              )}
               <span className={styles.innertext}>차수 관리</span>
             </div>
-          </li>
+          </div>
         </Link>
-      </ul>
+      </div>
     </div>
   );
 };
